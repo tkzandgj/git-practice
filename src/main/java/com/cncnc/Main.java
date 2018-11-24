64162c3ee91de09200c823a3609eba68bcb63320
tukangzheng <tukangzheng@163.com>
2018-11-24 14:41:43 +0800

    add

--- src/main/java/com/cncnc/Main.java
+++ src/main/java/com/cncnc/Main.java
@@ -64,5 +64,6 @@ public class Main {
     private static void test6(){
         System.out.println("this branch is test6");
         System.out.println("@@@@@");
+        System.out.println("#############");
     }
 }
