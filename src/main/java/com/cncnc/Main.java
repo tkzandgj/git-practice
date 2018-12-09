36bf1276b464f3e01c0f59898c1265730dd13d13
tukangzheng <tukangzheng@163.com>
2018-12-09 15:00:24 +0800

    修改代码

--- src/main/java/com/cncnc/Main.java
+++ src/main/java/com/cncnc/Main.java
@@ -40,6 +40,7 @@ public class Main {
         System.out.println("@@@@@@@@@@@@@@@@@@");
 
         System.out.println("===============");
+        System.out.println("---------------");
     }
 
     private static void test(){
