581f94aa72215680543c5f795e5193d5e26e3620
tukangzheng <tukangzheng@163.com>
2018-12-09 15:01:51 +0800

    再次修改代码

--- src/main/java/com/cncnc/Main.java
+++ src/main/java/com/cncnc/Main.java
@@ -40,7 +40,7 @@ public class Main {
         System.out.println("@@@@@@@@@@@@@@@@@@");
 
         System.out.println("===============");
-        System.out.println("---------------");
+        System.out.println("---------------dasdas");
     }
 
     private static void test(){
